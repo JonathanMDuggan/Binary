522a44302257565374278e8cdaa271beb128da79
JonathanMDuggan <JonathanDuggan2002@gmail.com>
2024-04-30 23:39:32 -0400

    doing this because I don't want anymore grey gaps in my profile... Made VulkanViewport pointers unique

--- src/gui/gb_gui_viewport.cpp
+++ src/gui/gb_gui_viewport.cpp
@@ -1,17 +1,33 @@
 #include"include/gb_gui.h"
-gbengine::VulkanViewport::VulkanViewport(gbVulkanGraphicsHandler vulkan) {
-  // Link the viewport to the vulkan instance 
-  logical_device_  = vulkan.device;
-  graphics_queue_  = vulkan.graphics_queue;
-  command_pool_    = vulkan.command_pool;
-  descriptor_pool_ = vulkan.descriptor_pool;
-  descriptor_set_layout_ = vulkan.descriptor_set_layout;
+gbengine::VulkanViewport::VulkanViewport(gbVulkanGraphicsHandler vulkan) :
+  logical_device_(vulkan.device),
+  graphics_queue_(vulkan.graphics_queue),
+  command_pool_(vulkan.command_pool),
+  descriptor_pool_(vulkan.descriptor_pool),
+  descriptor_set_layout_(vulkan.descriptor_set_layout) {
 
 }
 
 void gbengine::VulkanViewport::Destory() {
-  vkDestroySampler(*logical_device_, texture_sampler_, allocator_);
-  vkDestroyImageView(*logical_device_, texture_image_view_, allocator_);
-  vkDestroyImage(*logical_device_, texture_image_, allocator_);
-  vkFreeMemory(*logical_device_, texture_image_memory_, allocator_);
+  vkDestroySampler(*logical_device_, texture_sampler_, allocator_.get());
+  vkDestroyImageView(*logical_device_, texture_image_view_, allocator_.get());
+  vkDestroyImage(*logical_device_, texture_image_, allocator_.get());
+  vkFreeMemory(*logical_device_, texture_image_memory_, allocator_.get());
+}
+
+void gbengine::VulkanViewport::Free() {
+  vkFreeDescriptorSets(*logical_device_, *descriptor_pool_, 1,
+                       &descriptor_set_);
+}
+
+void gbengine::VulkanViewport::Update(void* array_data) {
+}
+
+void gbengine::VulkanViewport::LoadFromPath(const char* file_path) {
+}
+
+void gbengine::VulkanViewport::LoadFromArray(void* array_data,
+                                             VkDeviceSize array_size,
+                                             uint32_t w, uint32_t h){
+  
 }
--- src/gui/include/gb_gui.h
+++ src/gui/include/gb_gui.h
@@ -21,23 +21,30 @@ class VulkanViewport {
   VulkanViewport(gbVulkanGraphicsHandler vulkan);
   void Destory();
   void Free();
+  void Update(void* array_data);
   void LoadFromPath(const char* file_path);
   void LoadFromArray(void* array_data, VkDeviceSize array_size, uint32_t w,
                      uint32_t h);
+
+  VkDescriptorSet descriptor_set_;  
  private:
   uint32_t mips_levels_{};
   VkSampler texture_sampler_{};
   VkImageView texture_image_view_{};
   VkDeviceMemory texture_image_memory_{};
   VkImage texture_image_{};
+  VkDeviceSize array_size_;
+  uint32_t w_;
+  uint32_t h_;
 
-  // Pointers to vulkan logical device and its dependencies
-  VkAllocationCallbacks* allocator_ = VK_NULL_HANDLE;
-  VkDevice* logical_device_;
-  VkQueue* graphics_queue_;
-  VkCommandPool* command_pool_;
-  VkDescriptorPool* descriptor_pool_;
-  VkDescriptorSetLayout* descriptor_set_layout_;
+  // Pointers to vulkan logical device and its dependencies 
+  std::unique_ptr<VkAllocationCallbacks> allocator_ = VK_NULL_HANDLE;
+  std::unique_ptr<VkDevice> logical_device_;
+  std::unique_ptr<VkQueue> graphics_queue_;
+  std::unique_ptr<VkCommandPool> command_pool_;
+  std::unique_ptr<VkDescriptorPool> descriptor_pool_;
+  std::unique_ptr<VkDescriptorSetLayout> descriptor_set_layout_;
+  std::unique_ptr<VkDescriptorSetLayout> texture_descriptor_set_layout;
 };
 }  // namespace gbengine
 
